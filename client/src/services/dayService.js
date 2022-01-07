fe1ea4ccac4e6210573b943909acc0b7da796228
kfremgen97 <kfremgen97@gmail.com>
2022-01-06 19:01:49 -0500

    refractor: Update day service for api integration

--- client/src/services/dayService.js
+++ client/src/services/dayService.js
@@ -1,9 +1,18 @@
 // get day info
 const getDayInfo = function () {
-  return fetch('../assets/json/day.json')
-    .then((response) => response.json())
+  const url = 'http://localhost:8080/api/day/picture';
+  return fetch(url)
+    .then((response) => {
+      // check to make sure response is ok
+      if (!response) throw new Error('Unable to get picture of the day');
+      // return javascript object of json
+      return response.json();
+    })
     .then((data) => {
       console.log(data);
+      // make sure data doesnt have an error
+      if (data.error) throw new Error(data.error);
+      // return the data
       return data;
     })
     .catch((error) => {
